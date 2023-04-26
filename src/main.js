bbc52cbf4802642f77a8c5c5408bdaa37b7afedb
Jenrade112 <olorodeadebanke112@gmail.com>
2023-04-26 13:53:21 +0100

    commit my-app

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import react from 'react';
+
+function Main(){
+    return (
+        <main>
+            <section>
+                <h2>About us</h2>
+              <p>
+              We are a small team of web developers based in the United States. We
+         specialise in creating modern and responsive websites for businesses and individuals.
+
+
+                </p>  
+            </section>
+        </main>
+    );
+}
+export default Main;
